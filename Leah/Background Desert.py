f51e2ca0af9d9d5da24ecba2ae8c40cfbc31b4c0
Leah <leah.winters@ericsson.com>
2026-08-04 23:12:12 -1100

    m

--- Leah/Background Desert.py
+++ Leah/Background Desert.py
@@ -6,14 +6,19 @@ pygame.init()
 pygame.display.set_caption('Penguine Runner')
 clock = pygame.time.Clock()
 
-WIDTH, HEIGHT = 1280, 720
+WIDTH, HEIGHT = 1280, 700
 display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
 
-
+# sprite class
+class Desert(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(topleft=pos)
 # load image
 desert_image = pygame.image.load(join('Leah', 'Desert Image.png')).convert_alpha()
-desert_surf = pygame.transform.scale(desert_image, (1280, 720))
+desert_surf = pygame.transform.scale(desert_image, (1280, 700))
 
 # sprite class
 class Desert(pygame.sprite.Sprite):
--- Leah/Obstacal Classes.py
+++ Leah/Obstacal Classes.py
@@ -1,5 +1,45 @@
 import pygame 
+from os.path import join
+from random import randint
 
-class Cloud(pygame.sprite.Sprite):
+class Catus(pygame.sprite.Sprite):
     def __init__(self, surf, pos, groups):
         super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH),randint(0, WINDOW_HEIGHT)))
+
+
+pygame.init()
+pygame.display.set_caption('Penguine Runner')
+running = True
+clock = pygame.time.Clock()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+TOP_COLOR = (135, 206, 235)    
+BOTTOM_COLOR = (144, 238, 144)  
+TOP_HEIGHT = (WINDOW_HEIGHT * 3) // 4
+
+catus_surf = pygame.image.load(join('Leah', 'Happy Catus.png')).convert_alpha()
+# running = True
+
+all_sprites = pygame.sprite.Group()
+catus_sprites = pygame.sprite.Group()
+# for i in range(3):
+    # Catus(all_sprites, catus_surf)
+
+
+while running:
+    dt = clock.tick() / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            all_sprites.update(dt)
+    all_sprites.draw(screen)
+    pygame.draw.rect(screen, TOP_COLOR, (0, 0, WINDOW_WIDTH, TOP_HEIGHT))
+    pygame.draw.rect(screen, BOTTOM_COLOR, (0, TOP_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT - TOP_HEIGHT))
+    pygame.display.flip()
+   
+    pygame.display.update()
+
+pygame.quit()
